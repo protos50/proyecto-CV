322a1945476a41d5b78bb59c261f6d558e8825b1
protos50 <francojzini@gmail.com>
2023-01-23 23:58:35 -0300

    "html2pdf - scale page"

--- assets/js/main.js
+++ assets/js/main.js
@@ -98,27 +98,38 @@ themeButton.addEventListener('click', () => {
 })
 
 /*==================== REDUCE THE SIZE AND PRINT ON AN A4 SHEET ====================*/ 
-
+function scaleCv() {
+    document.body.classList.add('scale-cv');
+}
 
 /*==================== REMOVE THE SIZE WHEN THE CV IS DOWNLOADED ====================*/ 
-
+function removeScale() {
+    document.body.classList.remove('scale-cv');
+}
 
 /*==================== GENERATE PDF ====================*/ 
 // PDF generated area
+let areaCv = document.getElementById('area-cv');
+
+let resumeButton = document.getElementById('resume-button');
 
 
 // Html2pdf options
 
 
 // Function to call areaCv and Html2Pdf options 
-
+function generateResume() {
+    html2pdf(areaCv);
+}
 
 // When the button is clicked, it executes the three functions
-
+resumeButton.addEventListener('click', () => {
     // 1. The class .scale-cv is added to the body, where it reduces the size of the elements
-
+    scaleCv();
 
     // 2. The PDF is generated
-
+generateResume();
 
     // 3. The .scale-cv class is removed from the body after 5 seconds to return to normal size.
+
+})
